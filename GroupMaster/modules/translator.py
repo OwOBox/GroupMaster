a0c58baadf1dff15b1583aa8696ea95d241a2866
OwOBox <89490108+OwOBox@users.noreply.github.com>
2021-08-30 15:48:17 +0700

    Update translator.py

--- GroupMaster/modules/translator.py
+++ GroupMaster/modules/translator.py
@@ -23,7 +23,7 @@ def do_translate(bot: Bot, update: Update, args: List[str]):
         translated = translator.translate(to_translate_text, dest=lan)
         src_lang = translated.src
         translated_text = translated.text
-        msg.reply_text("*Dịch từ {} sang {}.*\n\n {}".format(src_lang, lan, translated_text))
+        msg.reply_text("**Dịch từ {} sang {}.**\n\n {}".format(src_lang, lan, translated_text))
     except :
         msg.reply_text("Lỗi")
 
