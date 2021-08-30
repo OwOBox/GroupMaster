d109c4f33f5fd43c1589df129b8980096c3d22ea
OwOBox <89490108+OwOBox@users.noreply.github.com>
2021-08-30 15:45:40 +0700

    Update translator.py

--- GroupMaster/modules/translator.py
+++ GroupMaster/modules/translator.py
@@ -23,13 +23,13 @@ def do_translate(bot: Bot, update: Update, args: List[str]):
         translated = translator.translate(to_translate_text, dest=lan)
         src_lang = translated.src
         translated_text = translated.text
-        msg.reply_text("Translated from {} to {}.\n {}".format(src_lang, lan, translated_text))
+        msg.reply_text("Dịch từ {} sang {}.\n {}".format(src_lang, lan, translated_text))
     except :
-        msg.reply_text("Error")
+        msg.reply_text("Lỗi")
 
 
-__help__ = """- /tr (language code) as reply to a long message.
+__help__ = """- /dich (language code) như trả lời một tin nhắn dài.
 """
-__mod_name__ = "Translator"
+__mod_name__ = "Dịch 💬"
 
-dispatcher.add_handler(DisableAbleCommandHandler("tr", do_translate, pass_args=True))
+dispatcher.add_handler(DisableAbleCommandHandler("dich", do_translate, pass_args=True))
