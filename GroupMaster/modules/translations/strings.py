a33963996cd4596785428800960f38410427f4e3
OwOBox <89490108+OwOBox@users.noreply.github.com>
2021-08-30 10:48:31 +0700

    Update strings.py

--- GroupMaster/modules/translations/strings.py
+++ GroupMaster/modules/translations/strings.py
@@ -1,26 +1,23 @@
 from GroupMaster.modules.sql.translation import prev_locale
 from GroupMaster.modules.translations.English import EnglishStrings
-from GroupMaster.modules.translations.Russian import RussianStrings
-from GroupMaster.modules.translations.Ukraine import UkrainianStrings
-from GroupMaster.modules.translations.Spanish import SpanishStrings
-from GroupMaster.modules.translations.Turkish import TurkishStrings
-from GroupMaster.modules.translations.Indonesian import IndonesianStrings
+from GroupMaster.modules.translations.Russian import VietnameseStrings
+
 
 def tld(chat_id, t, show_none=True):
     LANGUAGE = prev_locale(chat_id)
     print(chat_id, t)
     if LANGUAGE:
         LOCALE = LANGUAGE.locale_name
-        if LOCALE in ('ru') and t in RussianStrings:
-           return RussianStrings[t]
-        elif LOCALE in ('ua') and t in UkrainianStrings:
-            return UkrainianStrings[t]
-        elif LOCALE in ('es') and t in SpanishStrings:
-            return SpanishStrings[t]
-        elif LOCALE in ('tr') and t in TurkishStrings:
-            return TurkishStrings[t]
-        elif LOCALE in ('id') and t in IndonesianStrings:
-            return IndonesianStrings[t]
+        if LOCALE in ('vi') and t in VietnameseStrings:
+           return VietnameseStrings[t]
+#        elif LOCALE in ('ua') and t in UkrainianStrings:
+#            return UkrainianStrings[t]
+#        elif LOCALE in ('es') and t in SpanishStrings:
+#            return SpanishStrings[t]
+#        elif LOCALE in ('tr') and t in TurkishStrings:
+ #           return TurkishStrings[t]
+#        elif LOCALE in ('id') and t in IndonesianStrings:
+#            return IndonesianStrings[t]
         else:
             if t in EnglishStrings:
                 return EnglishStrings[t]
@@ -44,16 +41,16 @@ def tld_help(chat_id, t):
 
         print("Test2", t)
 
-        if LOCALE in ('ru') and t in RussianStrings:
-            return RussianStrings[t]
-        elif LOCALE in ('ua') and t in UkrainianStrings:
-            return UkrainianStrings[t]
-        elif LOCALE in ('es') and t in SpanishStrings:
-            return SpanishStrings[t]
-        elif LOCALE in ('tr') and t in TurkishStrings:
-            return TurkishStrings[t]
-        elif LOCALE in ('id') and t in IndonesianStrings:
-            return IndonesianStrings[t]
+        if LOCALE in ('vi') and t in VietnameseStrings:
+            return VietnameseStrings[t]
+  #      elif LOCALE in ('ua') and t in UkrainianStrings:
+  #          return UkrainianStrings[t]
+  #      elif LOCALE in ('es') and t in SpanishStrings:
+  #          return SpanishStrings[t]
+  #      elif LOCALE in ('tr') and t in TurkishStrings:
+  #          return TurkishStrings[t]
+  #      elif LOCALE in ('id') and t in IndonesianStrings:
+  #          return IndonesianStrings[t]
         else:
             return False
     else:
